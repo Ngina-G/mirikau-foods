460a08da35413486a73d5cb83712de9d7d2d5721
Ngina <Nginagitau11@outlook.com>
2022-05-29 11:30:57 +0300

    fix: adds correct path joining syntax

--- mirikaufoods/settings.py
+++ mirikaufoods/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
 # Media
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
