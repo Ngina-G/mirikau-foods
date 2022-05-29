7185d676ce2d812caf9f8592b7b98dee95193a3b
Ngina <Nginagitau11@outlook.com>
2022-05-29 11:31:13 +0300

    update: adds location test

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Location, Category, Image
 
 # Create your tests here.
+class LocationTestClass(TestCase):
+    '''
+        Set up method
+    '''
+    def setUp(self):
+        self.london= Location(name='London')
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.london, Location))
