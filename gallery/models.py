8a4712da3d3f350733944de041555b237877ec1e
Ngina <Nginagitau11@outlook.com>
2022-05-29 11:12:03 +0300

    update: adds the three models and the initial migration

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=35)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=35)
+
+    def __str__(self):
+        return self.name
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'gallery/')
+    image_name = models.CharField(max_length=50)
+    image_description = models.CharField(max_length=255)
+    image_location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    image_category = models.ForeignKey(Category, on_delete=models.CASCADE)
