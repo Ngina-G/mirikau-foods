ac31b6a14b980e75194be7035fda2564f5ff9826
Ngina <Nginagitau11@outlook.com>
2022-05-29 10:25:55 +0300

    update: connects to postgres

--- gallery/views.py
+++ gallery/views.py
@@ -4,3 +4,17 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(home)
+
+    return render(request, 'gallery/index.html')
