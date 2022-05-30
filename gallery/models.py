080acd3fafdc62f611a7a531ce0871a3faedb4ce
Ngina <Nginagitau11@outlook.com>
2022-05-30 15:52:41 +0300

    update: adds the save method

--- gallery/models.py
+++ gallery/models.py
@@ -7,11 +7,17 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
+    def save_location(self):
+        self.save()
+
 class Category(models.Model):
     name = models.CharField(max_length=35)
 
     def __str__(self):
         return self.name
+
+    def save_category(self):
+        self.save()
 
 class Image(models.Model):
     image = models.ImageField(upload_to = 'gallery/')
