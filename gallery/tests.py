923a81207ba21c5657189d90a9527faf46cd09fe
Ngina <Nginagitau11@outlook.com>
2022-05-30 15:51:02 +0300

    update: adds failed save method test

--- gallery/tests.py
+++ gallery/tests.py
@@ -12,3 +12,32 @@ class LocationTestClass(TestCase):
     # Testing instance
     def test_instance(self):
         self.assertTrue(isinstance(self.london, Location))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.london.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)> 0)
+
+    # def test_delete_method(self):
+    #     self.london.delete_location(id)
+    #     locations = Location.objects.all()
+    #     self.assertTrue(len(locations)< 0)
+
+class CategoryTestClass(TestCase):
+    '''
+        Set up method
+    '''
+    def setUp(self):
+        self.food= Category(name='Food')
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.food, Category))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.food.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories)> 0)
+
